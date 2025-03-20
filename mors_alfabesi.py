ac8bae202108b055d9e8dc9052321a7424267370
Bünyamin Epatay <epatayb@gmail.com>
2025-03-20 16:33:56 +0300

    Uygulamanın ana yapısı kurgulandı test edildi.

--- mors_alfabesi.py
+++ mors_alfabesi.py
@@ -1,39 +1,40 @@
 from encryption import encrypt_text
+from decryption import decrypt_text
 
-# Mors sözlüğü
-morse_dict = {
-    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
-    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
-    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
-    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
-    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
-    'Z': '--..', 'Ö': '---.', 'Ü': '..--', 'Ç': '----', 'Ş': '...-.', '0': '-----', '1': '.----', '2': '..---', '3': '...--',
-    '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
-    '9': '----.', ' ': ' '
-}
 
-def decrypt(mors_code):
-    mors_code = mors_code.strip()
-    morse_words = mors_code.split('  ')
-    text = ""
+def main():
+    # Mors sözlüğü
+    morse_dict = {
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+        'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+        'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+        'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+        'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+        'Z': '--..', 'Ö': '---.', 'Ü': '..--', 'Ç': '----', 'Ş': '...-.', 
+        '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+        '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+        '9': '----.', ' ': ' '
+    }
 
-    for morse_word in morse_words:
-        morse_characters = morse_word.split(' ')
-        word = ""
-        for code in morse_characters:
-            for key, value in morse_dict.items():
-                if code == value:
-                    word += key
-        text += word + ' '
+    while(True):
+        print("\n Morse Kodu Uygulaması")
+        print("1 - Şifreleme (Düz metni Morse koduna çevirin)")
+        print("2 - Şifre Çözme (Morse Kodunu düz metne çevirin)")
+        print("3 - Çıkış")
 
-    return text.strip()
+        choice = input("Seçiminizi Yapın: ")
 
-text = input("Şifrelenecek metni girin: ")
-print("Morse Kodu:", encrypt_text(text.upper(), morse_dict))
+        if choice == '1':
+            text = input("Şifrelenecek Metin: ")
+            print(encrypt_text(text.upper(),morse_dict))
+        elif choice == '2':
+            text = input('Çözümlenecek Morse kodu girin: ')
+            print(decrypt_text(text.upper(), morse_dict))
+        elif choice == '3':
+            print("Çıkış yapılıyor...")
+            break
+        else:
+            print("Geçersiz seçim! Lütfen tekrar deneyin.")
 
-# decoded_text = decrypt(mors_code)
-# print("Çözülmüş Metin:", decoded_text)
-
-# encrypted_text = input("Şifreli metni giriniz: ")
-# original_text = decrypt(encrypted_text)
-# print("Orjinal metin: ", original_text)
+if __name__ == "__main__":
+    main()
